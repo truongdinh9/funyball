d5b9b7b23e937094908ff90b814f150c556b430e
dinh.truon <dinh.kttn.neu@gmail.com>
2019-03-07 20:35:59 +0700

    vẽ được hình tròn màu blue đơn giản

--- funyball.js
+++ funyball.js
@@ -1,5 +1,13 @@
+ function Circle(x,y,radius) {
+
+ }
 var canava = document.getElementById("ballcanvas");
 var ctx = canava.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(600,100);
-ctx.stroke();
+function CreatCircle() {
+    ctx.beginPath();
+    ctx.arc(50, 50, 50, 0, 2 * Math.PI, true);
+    ctx.fillStyle = "Blue";
+    ctx.fill();
+    ctx.stroke();
+}
+CreatCircle();
