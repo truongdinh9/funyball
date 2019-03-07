ee6864e0fc3d12fc7468a8698c95e3d692fce8af
dinh.truon <dinh.kttn.neu@gmail.com>
2019-03-07 23:17:06 +0700

    vẽ đc 2 hình tròn với màu random

--- funyball.js
+++ funyball.js
@@ -1,13 +1,19 @@
- function Circle(x,y,radius) {
-
+ function Ball(x,y,radius) {
+    this.x =x
+    this.y=y
+    this.radius=radius
+     this.colorr=["Red","Blue","Green","Violet"][Math.floor(Math.random()*3)]
  }
 var canava = document.getElementById("ballcanvas");
 var ctx = canava.getContext("2d");
-function CreatCircle() {
+function CreatCircle(a) {
     ctx.beginPath();
-    ctx.arc(50, 50, 50, 0, 2 * Math.PI, true);
-    ctx.fillStyle = "Blue";
+    ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI, true);
+    ctx.fillStyle = a.colorr;
     ctx.fill();
     ctx.stroke();
 }
-CreatCircle();
+let Ball1=new Ball(50,50,50)
+CreatCircle(Ball1);
+ let Ball2=new Ball(500,100,100)
+ CreatCircle(Ball2);
