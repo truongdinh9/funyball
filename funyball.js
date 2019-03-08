5b2e6a44183ca1a8f8141653c3d51eb703821402
dinh.truon <dinh.kttn.neu@gmail.com>
2019-03-08 15:41:52 +0700

    vẽ dc n hình đã chạy

--- funyball.js
+++ funyball.js
@@ -1,11 +1,15 @@
- function Ball(x,y,radius) {
-    this.x =x
-    this.y=y
-    this.radius=radius
-     this.colorr=["Red","Blue","Green","Violet"][Math.floor(Math.random()*3)]
- }
+function Ball(x, y, radius) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.colorr = ["Red", "Blue", "Green", "Violet", "Orange", "Yellow"][Math.floor(Math.random() * 6)]
+    this.vx = 2;
+    this.vy = 2;
+}
+
 var canava = document.getElementById("ballcanvas");
 var ctx = canava.getContext("2d");
+
 function CreatCircle(a) {
     ctx.beginPath();
     ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI, true);
@@ -13,7 +17,31 @@ function CreatCircle(a) {
     ctx.fill();
     ctx.stroke();
 }
-let Ball1=new Ball(50,50,50)
-CreatCircle(Ball1);
- let Ball2=new Ball(500,100,100)
- CreatCircle(Ball2);
+
+let a = [];
+let N = parseInt(prompt("Nhập số quả bóng"));
+
+for (let i = 0; i < N; i++) {
+    a[i] = new Ball(Math.floor(Math.random() * 10 + 1) * 110, Math.floor(Math.random() * 10 + 1) * 45, 10);
+}
+
+// let a=new Ball(Math.floor(Math.random()*10+1)*110,Math.floor(Math.random()*10+1)*45,10);
+function chay(a) {
+
+    setTimeout(function () {
+        ctx.clearRect(0, 0, 1200, 500);
+        for (let i=0;i<N;i++){
+            CreatCircle(a[i]);
+            if (a[i].x < a[i].radius || a[i].x > 1200 - a[i].radius) {
+            a[i].vx = -a[i].vx
+        }
+        if (a[i].y < a[i].radius || a[i].y > 500 - a[i].radius) {
+            a[i].vy = -a[i].vy
+        }
+        a[i].x += a[i].vx;
+        a[i].y += a[i].vy;
+        }
+        chay(a);
+    }, 10)
+}
+chay(a);
